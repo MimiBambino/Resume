35305c406bc604db0497729b7371b7e6789cf901
Cynthia O'Donnell <mimibambino@gmail.com>
2014-10-30 16:32:54 +0100

    first commit

---
+++ js/helper.js
@@ -0,0 +1,76 @@
+
+var HTMLheaderName = "<h1 id='name' class='analgous-color'>%data%</h1>";
+var HTMLheaderRole = "<h4 class='contrasting1-color'>%data%</h4>";
+
+var HTMLcontactGeneric = "<li><span class='orange-text'>%contact%</span><span class='analgous-color'>%data%</span></li>";
+var HTMLmobile = "<li><span class='orange-text'>mobile</span><span class='analgous-color'>%data%</span></li>";
+var HTMLemail = "<li><span class='orange-text'>email</span><span class='analgous-color'>%data%</span></li>";
+var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='analgous-color'>%data%</span></li>";
+var HTMLgithub = "<li><span class='orange-text'>github</span><span class='analgous-color'>%data%</span></li>";
+var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='analgous-color'>%data%</span></li>";
+var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='analgous-color'>%data%</span></li>";
+
+var HTMLbioPic = "<div id='photo'><img class='img-responsive img-rounded' src='%data%' alt='Picture of Cynthia O&#8217;Donnell'></div>";
+var HTMLwelcome = "<p>%data%</p>";
+
+var HTMLskillsStart = "<ul id='skills' class='box'>";
+var HTMLskills = "<li><span class='analgous-color'>%data%</span></li>";
+var HTMLskillsEnd = "</ul>"
+
+var HTMLworkStart = "<div class='work-entry'></div>";
+var HTMLworkEmployer = "<h3>%data%";
+var HTMLworkTitle = " - %data%</h3>";
+var HTMLworkDates = "<div class='date-text'>%data%</div>";
+var HTMLworkLocation = "<div class='location-text'>%data%</div>";
+var HTMLworkDescription = "<p><br>%data%</p>";
+
+var HTMLprojectStart = "<div class='project-entry'></div>";
+var HTMLprojectTitle = "<a href='%url%' target='_blank'><h3>%data%</h3></a>";
+var HTMLprojectDates = "<div class='date-text'>%data%</div>";
+var HTMLprojectDescription = "<p><br>%data%</p>";
+var HTMLprojectImage = "<a href='%url%' target='_blank'><img class='img-responsive img-thumbnail' src='%data%'></a>";
+
+var HTMLschoolStart = "<div class='education-entry'></div>";
+var HTMLschoolName = "<a href='%url%'><h3>%data%</h3></a>";
+var HTMLschoolDegree = " -- %data%";
+var HTMLschoolDates = "<div class='date-text'>%data%</div>";
+var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
+var HTMLschoolMajor = "<em><br>Major: %data%</em>";
+var HTMLschoolURL = "<br><a href='#'>%data%</a>";
+
+var HTMLonlineClasses = "<h2>Online Classes</h2>";
+var HTMLonlineTitle = "<a href='%url'%><h3>%data%</h3></a>";
+var HTMLonlineSchool = " -- %data%";
+var HTMLonlineDates = "<div class='date-text'>%data%</div>";
+var HTMLonlineCertificate = "<br><a class='certificate' href='%certificate%'>Certificate</a>";
+
+var contactLocation = "<li class='orange-text contact-type'>location: <span class='analgous-color contact-data'>%data%</span></li>";
+var contactWebsite = "<li class='orange-text contact-type'>website: <span class='analgous-color contact-data'><a target='_blank' href='http://%data%'>%data%</a></span></li>";
+var contactEmail = "<li class='orange-text contact-type'>email: <span class='analgous-color contact-data'><a href='mailto:%data%'>%data%</a></span></li>";
+var contactTwitter = "<li class='orange-text contact-type'>twitter: <span class='analgous-color contact-data'><a target='_blank' href='http://www.twitter.com/mimibambino'>%data%</a></span></li>";
+var contactGithub = "<li class='orange-text contact-type'>github: <span class='analgous-color contact-data'><a target='_blank' href='https://www.github.com/%data%'>%data%</a></span></li>";
+var contactLinkedIn = "<li class='orange-text contact-type'>linkedin: <span class='analgous-color contact-data'><a target='_blank' href='http://www.linkedin.com/pub/cynthia-o-donnell/a2/719/7a0/'>%data%</a></span></li>";
+
+var HTMLlanguage = "<li><span class='analgous-color'>%data%</span></li>";
+
+var googleMap = "<div id='map' class='box'></div>";
+
+/*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+*/
+clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      "x": x,
+      "y": y
+    }
+  );
+  console.log("x location: " + x + "; y location: " + y);
+}
+
+$(document).click(function(loc) {
+  // your code goes here!
+});
+
---
+++ js/resumeBuilder.js
@@ -0,0 +1,344 @@
+var work = {
+  "jobs": [
+  {
+    "job": "lawyer",
+    "employer": "Case, Lombardi &amp; Pettit",
+    "title": "Attorney",
+    "location": "Honolulu, Hawaii",
+    "dates": "2008 - 2010",
+    "description": "I began my legal career as a bankruptcy and business litigation attorney during the Summer of 2008. About 2 months after I began my career, the financial crisis hit. As a result, I was quickly indoctrinated in the inner workings of business reorganization, corporate board restructuring, and corporate finance as I advised and represented business clients on their rights under federal bankruptcy law as corporate creditors."
+  },
+  {
+    "job": "navy",
+    "employer": "United States Navy",
+    "title": "Naval Officer",
+    "location": "Norfolk, Virginia",
+    "dates": "2001 - 2005",
+    "description": "As a U.S. Navy Surface Warfare Officer onboard a Guided Missile Destroyer I supervised and trained a division of 20 Sailors. Additionally, I oversaw the maintenance and use of all onboard missile launching system hardware (including missiles) and installation and use of all system software.  I served in the Arabian Gulf, Indian Ocean, Mediterranean Sea and the North Sea."
+  }
+  ],
+    "display": function() {
+      for (i in work.jobs) {
+      $("#workExperience").append(HTMLworkStart);
+      var job = work.jobs[i];
+      var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+      var formattedEmployerTitle = formattedEmployer + formattedTitle;
+      $(".work-entry:last").append(formattedEmployerTitle);
+  
+      var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+      var formattedjobLocation = HTMLworkLocation.replace("%data%", job.location);
+      var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+      $(".work-entry:last").append(formattedDates);
+      $(".work-entry:last").append(formattedjobLocation);
+      $(".work-entry:last").append(formattedDescription);
+    }
+  }
+};
+
+var projects = {
+    "project": [
+        {
+            "title": "Brigitte",
+            "dates": "2012",
+            "description": "A sweet little girl",
+            "images": "images/Brigitte.jpg", 
+            "url": "https://github.com/MimiBambino"
+        },
+        {
+            "title": "Interactive Resume",
+            "dates": "2014",
+            "description": "A resume build with JavaScript, jQuery and Bootstrap.",
+            "images": "images/IR.jpg",
+            "url": "https://github.com/MimiBambino/InteractiveResume"
+        },
+        {
+            "title": "Johnny",
+            "dates": "2010",
+            "description": "This kid loves Spiderman!",
+            "images": "images/Johnny.jpg",
+            "url": "https://github.com/MimiBambino"
+        } ],
+        "display": function () {
+          for (i in projects.project) {
+            $("#projects").append(HTMLprojectStart);
+            var project = projects.project[i];
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title).replace('%url%', project.url);
+            $(".project-entry:last").append(formattedProjectTitle);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
+            $(".project-entry:last").append(formattedProjectDates);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
+            $(".project-entry:last").append(formattedProjectDescription);
+            var formattedProjectImage = HTMLprojectImage.replace("%data%", project.images).replace('%url%', project.url);
+            $(".project-entry:last").append(formattedProjectImage);
+          }
+        }
+};
+
+  var education = {
+  "schools": [
+    {
+    "name": "University of Florida",
+    "location": "Gainesville, Florida",
+    "degree": "Juris Doctor", 
+    "major": "Law",
+    "dates": "2005 &#151; 2008",
+    "url": "http://www.law.ufl.edu/"
+    },
+    {
+    "name": "Florida State University",
+    "location": "Tallahassee, Florida",
+    "degree": "Bachelor of Arts",
+    "major": "Music",
+    "dates": "1996 &#151; 2000",
+    "url": "http://www.music.fsu.edu//"
+    }
+  ],
+  "onlineCourses": [
+    {
+    "title": "Front End Developer Nanodegree",
+    "school": "Udacity",
+    "dates": 2014,
+    "url": "http://www.udacity.com",
+    "certificate": ""
+    },
+    {
+    "title": "Introduction to Linux",
+    "school": "edX",
+    "dates": 2014,
+    "url": "https://courses.edx.org/courses/LinuxFoundationX/LFS101x/2T2014/info",
+    "certificate": "images/IntroToLinuxCertificate.pdf"
+    },
+    {
+    "title": "Engineering Software as a Service",
+    "school": "edX",
+    "dates": 2014,
+    "url": "https://courses.edx.org/courses/BerkeleyX/CS_CS169.1x/1T2014/info",
+    "certificate": "images/SaasCertificate.pdf"
+    }
+  ],
+  "display": function(){
+
+    // Display Schools
+     for (i in education.schools) {
+     $("#education").append(HTMLschoolStart);
+     var school = education.schools[i];
+     var formattedschoolName = HTMLschoolName.replace("%data%", school.name);
+     $(".education-entry:last").append(formattedschoolName);
+     var formattedschoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+     $(".education-entry:last").append(formattedschoolDegree);
+     var formattedschoolDates = HTMLschoolDates.replace("%data%", school.dates);
+     $(".education-entry:last").append(formattedschoolDates);
+     var formattedschoolMajor = HTMLschoolMajor.replace("%data%", school.major);
+     $(".education-entry:last").append(formattedschoolMajor);
+     var formattedschoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+     $(".education-entry:last").append(formattedschoolLocation);
+    }
+
+    // Display Online Courses
+    $(".education-entry:last").append(HTMLonlineClasses);
+    for (var i in education.onlineCourses) {
+      var course = education.onlineCourses[i];
+      var formattedonlineTitle =  HTMLonlineTitle.replace("%data%", course.title).replace("%url%", course.url);
+      $(".education-entry:last").append(formattedonlineTitle);
+      var formattedonlineSchool =  HTMLonlineSchool.replace("%data%", course.school);
+      $(".education-entry:last").append(formattedonlineSchool);
+      var formattedonlineDates =  HTMLonlineDates.replace("%data%", course.dates);
+      $(".education-entry:last").append(formattedonlineDates);
+      if (course.certificate){
+        var formattedCertificate =  HTMLonlineCertificate.replace("%certificate%", course.certificate);
+        $(".education-entry:last").append(formattedCertificate);
+      }
+    }
+  }
+};
+
+var bio = {
+  "name": "Cynthia O'Donnell",
+  "role": "Front End Web Developer",
+  "aboutDeveloper": "Why become a Front End Web Developer?  Well, I've loved the internet since I first experienced it in the 90s. It felt like I suddenly had access to all of the information I could have ever wanted.  Twenty years later, it's even more true. However, I have been growing increasingly critical about how that information is organized.  I started learning about computer programming and web development because I wanted to help make the information already available online more useful by building web applications that find and display data in a more user-friendly way.",
+  "aboutMimi": "What is MimiBambino? It makes me smile. When my son was a baby, he called me Mimi. So for a few years, I <em>was</em> Mimi. He was the <em>bambino</em>. Now no one calls me Mimi and he is no longer a baby. To my delight, when I started creating handles and user accounts, I found that MimiBambino was always available. MimiBambino reminds me of that sweet, tender time when for most of the day it was just me and my baby.",
+    "languages" : [
+          "English",
+          "German"
+    ],
+  "contacts": [
+    {
+    "website" : "www.mimibambino.com", 
+    "email" : "mimibambino@gmail.com",
+    "twitter" : "@mimibambino",
+    "github" : "MimiBambino",
+    "linkedin" : "Cynthia O'Donnell",
+    "location" : "Pozzuoli, Italy"
+  }
+  ],
+  "skills": ["css", "php", "html5", "python", "javascript", "business law", "management", "paper airplanes"], 
+  "image": "images/Cynthia.jpg",
+  "display": function() {
+    // Display Skills
+    $("#skillsChart").append(HTMLskillsStart);
+    for (var i in bio.skills){
+      var skill = bio.skills[i];
+      var formattedSkill = HTMLskills.replace("%data%", skill);
+      $("#skills").append(formattedSkill);
+    }
+    $("#skills").append(HTMLskillsEnd);
+
+    // Display About Me
+    var formattedDeveloper = HTMLwelcome.replace("%data%", bio.aboutDeveloper);
+    $("#welcome").append(formattedDeveloper);
+    var formattedMimi = HTMLwelcome.replace("%data%", bio.aboutMimi);
+    $("#welcome").append(formattedMimi);
+
+    // Display Picture
+    var formattedPicture = HTMLbioPic.replace("%data%", bio.image);
+    $("#Pic").append(formattedPicture);
+
+    // Display Header
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#logo-header").append(formattedName);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#logo-header").append(formattedRole);
+
+    // Display Contacts
+    for (var i in bio.contacts){
+      var contact = bio.contacts[i];
+      var formattedWebsite = contactWebsite.replace("%data%", contact["website"]).replace("%data%", contact["website"]);
+      $("#Contacts").append(formattedWebsite);
+      var formattedEmail = contactEmail.replace("%data%", contact["email"]).replace("%data%", contact["email"]);
+      $("#Contacts").append(formattedEmail);
+      var formattedTwitter = contactTwitter.replace("%data%", contact["twitter"]);
+      $("#Contacts").append(formattedTwitter);
+      var formattedGithub = contactGithub.replace("%data%", contact["github"]).replace("%data%", contact["github"]);
+      $("#Contacts").append(formattedGithub);
+      var formattedLinkedIn = contactLinkedIn.replace("%data%", contact["linkedin"]);
+      $("#Contacts").append(formattedLinkedIn);
+      var formattedLocation = contactLocation.replace("%data%", contact["location"]);
+      $("#Contacts").append(formattedLocation);
+
+    // Display Languages
+    var formattedLanguage = HTMLlanguage.replace("%data%", bio.languages[0]);
+    $("#languages").append(formattedLanguage);
+    formattedLanguage = HTMLlanguage.replace("%data%", bio.languages[1]);
+    $("#languages").append(formattedLanguage);
+    }
+  }
+};
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+//Map
+
+  $("#mapDiv").append(googleMap);
+  
+  var map;   
+
+
+  function initializeMap() {
+
+   var locations;        
+
+   var mapOptions = {
+   zoomControl: true,
+   disableDefaultUI: true
+  };
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+  function locationFinder() {
+       var locations = [];
+
+      for (var biolocation in bio.contacts) {
+      locations.push(bio.contacts[biolocation].location);
+    }
+
+    for (var school in education.schools) {
+      locations.push(education.schools[school].location);
+    }
+
+    for (var job in work.jobs) {
+      locations.push(work.jobs[job].location);
+    }
+
+    return locations;
+  };
+
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.k;  // latitude from the place service
+    var lon = placeData.geometry.location.B;  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+    
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.open(map, marker);
+      });
+
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    map.fitBounds(bounds);
+    map.setCenter(bounds.getCenter());
+  };
+
+ callback = function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0])
+    }
+  };
+
+  function pinPoster(locations) {
+
+    var service = new google.maps.places.PlacesService(map);
+    for (place in locations) {
+
+      var request = {
+        query: locations[place]
+      }
+
+      service.textSearch(request, callback);
+    }
+  };
+
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  locations = locationFinder();
+
+  pinPoster(locations);
+  
+};
+
+window.addEventListener('load', initializeMap);
+
+window.addEventListener('resize', function(e) {
+
+map.fitBounds(mapBounds);
+});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
